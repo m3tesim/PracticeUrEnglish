530d9cbe043c50df79919f5fddf6d4196755f489
Moatsem <m3tsmsh@gmail.com>
2022-08-31 09:40:26 +0200

    creating the main UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main-container">
+
+      <section >
+        <h1 className="title">Choose the right answer that describe this word</h1>
+        <div className="main-word"> <h2>"Word"</h2></div>
+      </section>
+      <section>
+        <form>
+          <div>
+          <input name="type" id="noun" type="radio" value="noun"/>
+          <label  for="noun" > Noun</label>
+          <input name="type" id="verb" type="radio" value="verb"/>
+          <label  for="verb" > Verb</label>
+          <input name="type" id="adjective" type="radio" value="adjective"/>
+          <label  for="adjective" > Adjective</label>
+          <input name="type" id="adverb" type="radio" value="adverb"/>
+          <label  for="adverb" > Adverb</label>
+          </div>
+          <div>
+          <button type="submit" className="btn-confirm"> Confirm </button>
+          </div>
+        </form>
+      </section>
+      
     </div>
+    <footer> 
+Progress: 
+<span ></span>
+<span ></span>
+<span ></span>
+<span ></span>
+<span ></span>
+<span ></span>
+<span ></span>
+<span ></span>
+<span ></span>
+<span ></span>
+        
+    </footer>
+    </>
   );
 }
 
